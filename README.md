f081fdd402b7e06e704e105d80d460bc106105d4
Ian Balderston <ianmb@vt.edu>
2024-01-31 10:54:44 -0500

    two solutions added

--- README.md
+++ README.md
@@ -5,10 +5,15 @@ A visitor receives a prompt upon opening the website to enter a list of comma-se
 1. prompt in browser (enter list of comma-seperated froyo flavors)
 2. ?fill prompt with? -vanilla,vanilla,vanilla,strawberry,coffee,coffee
 -object with flavors entered and amount of each
+    -assign array with prompt entries
+    -empty object 'order'
+    -if (item[i] === )
     -loop here?
     -make a function that enters user input into object (flavor, amount)
         -parse into array then into object?
 3. console.table froyo order (flavors, +how many they ordered)
 
-?use loop to iterate through array of flavors
--object 
+
+TWO SOLUTIONS:
+1. start with object with flavor values: 0
+2. start with empty object, add values as each flavor is iterated through(if value = undefined{ make value = 1})
--- index.js
+++ index.js
@@ -1,2 +1,53 @@
-let froyoOrder = prompt('Please enter your order below!' )
+const userInputOrder = prompt(
+    "Please enter order below - separated by commas!",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  );
 
+const stringArrayInput = userInputOrder.split(",");
+
+const order = {}
+
+function updateOrder(array) {
+    for (const item of array) {
+        
+        if (order[item] === undefined) {
+            order[item] = 1;
+        } else {
+            order[item] += 1
+        }
+    }
+    return order
+}
+
+console.table(updateOrder(stringArrayInput))
+
+//ANOTHER POSSIBLE SOLUTION: 
+
+// const userInputOrder = prompt(
+//     "Please enter order below - separated by commas!",
+//     "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+//   );
+
+//   const stringArrayInput = userInputOrder.split(",");
+
+// const order = {
+//     vanilla: 0,
+//     coffee: 0,
+//     strawberry: 0
+// } 
+// function updateOrder(array) {
+//     for (const item of array) {
+//         if(item === 'vanilla') {
+//             order.vanilla += 1
+//         }
+//         if(item === 'coffee') {
+//             order.coffee += 1
+//         }
+//         if(item === 'strawberry') {
+//             order.strawberry += 1
+//         }
+//     }
+//     return order
+// }
+ 
+// console.table(updateOrder(stringArrayInput))
